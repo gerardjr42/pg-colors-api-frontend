8ccffb526850e67d7af8226649f651e37acc3c1d
gerardjr42 <ggarciajr12@gmail.com>
2024-07-24 01:45:54 -0400

    create fetch request to backend for index page

--- src/Components/Colors.jsx
+++ src/Components/Colors.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Color from "./Color";
 
 const API = import.meta.env.VITE_API_URL;
 function Colors() {
   const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/colors`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => {
+        setColors(responseJSON);
+      })
+      .catch((error) => console.error(error))
+  }, []);
 
   return (
     <>
